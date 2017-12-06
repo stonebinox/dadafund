973ae1b8d5264a65dd1a88556f0fb37a425a08ca
stonebinox <anoop.santhanam@gmail.com>
2017-12-06 14:26:47 +0530

    Managing errors.

--- web/js/dadafund.js
+++ web/js/dadafund.js
@@ -9,6 +9,22 @@ function readParams(){
             case "INVALID_USER_CREDENTIALS":
             err='Invalid credentials. Please verify the details and try again.';
             break;
+            case "INVALID_USER_NAME":
+            err='Invalid user name. Please enter your full name and try again.';
+            break;
+            case "INVALID_USER_EMAIL":
+            err='Invalid email ID. Please enter a valid email ID and try again.';
+            break;
+            case "INVALID_PASSWORD":
+            err='Invalid password. Please ensure the password is at least 8 characters in length.';
+            break;
+            case "PASSWORD_MISMATCH":
+            err='Password mismatch. Please ensure the passwords match each other.';
+            break;
+            case "ACCOUNT_ALREADY_EXISTS":
+            err='Account already exists. <a href="https://dusthq-dadafund.herokuapp.com/">Login</a> to your account.';
+            break;
+            
         }
         $("#message").html('<div class="alert alert-danger"><strong>Error</strong> '+err+'</div>');
     }
