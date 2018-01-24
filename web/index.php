7416b3555823e2d9f02f62b46e11d5a896dcb2ff
Anoop S <anoop.santhanam@gmail.com>
2018-01-24 21:12:45 +0530

    Added transaction

--- web/index.php
+++ web/index.php
@@ -7,6 +7,18 @@ require __DIR__.'/../config/prod.php';
 require __DIR__.'/../src/controllers.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+function secure($string)
+{
+    return addslashes(htmlentities($string));
+}
+function validate($data)
+{
+    if(($data!="")&&($data!=NULL))
+    {
+        return true;
+    }
+    return false;
+}
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => 'php://stderr',
 ));
